fbd842dd856b78b4b0b420cafddee58254e069c8
schnnils <schnnils@kkg-do.eu>
2025-05-16 10:18:21 +0200

    room2.js

---
+++ JS/room2.js
@@ -0,0 +1,4 @@
+// JavaScript Code für den Alert
+window.onload = function() {
+    alert("Hallo, du hast es in Raum 2 geschafft,es erwartet dich aber jetzt ein weiteres Rätsel!Was ist Herr AmBROsius Lieblingsfarbe?");
+};
