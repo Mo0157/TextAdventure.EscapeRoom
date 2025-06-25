185fd1c7469557cc8a261d56907b65c289632bdb
Matrix207 <allamatt@kkg-do.eu>
2025-06-25 20:20:49 +0200

    set position button room3.css

--- JS/room3.js
+++ JS/room3.js
@@ -4,15 +4,13 @@ function positioniereButtons() {
 
     function unsichtbarMachen(button) {
         button.style.position = 'absolute';
-            button.style.width = '100px';
-        button.style.height = '100px';
-        button.style.backgroundColor = 'transparent'; // unsichtbar
+        button.style.backgroundColor = 'transparent';
         button.style.border = 'none';
-        button.style.opacity = '0'; // komplett unsichtbar
+        button.style.opacity = '0';
         button.style.cursor = 'pointer';
     }
 
-    // Positionen setzen und unsichtbar machen
+
     mappe.style.top = '100px';
     mappe.style.left = '50px';
     mappe.style.position = 'absolute';
