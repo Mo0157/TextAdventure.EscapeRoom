c27e186392be3e1568b7e85d575fe9931c5b2f95
Matrix207 <allamatt@kkg-do.eu>
2025-06-25 20:07:48 +0200

    fehlerrrr

--- JS/room3.js
+++ JS/room3.js
@@ -2,6 +2,16 @@ alert ("Du hast es in den dritten Raum geschafft! Du bist in einem Labor und sie
 function positioniereButtons() {
     const mappe = document.getElementById('mappe');
 
+    function unsichtbarMachen(button) {
+        button.style.position = 'absolute';
+            button.style.width = '100px';
+        button.style.height = '100px';
+        button.style.backgroundColor = 'transparent'; // unsichtbar
+        button.style.border = 'none';
+        button.style.opacity = '0'; // komplett unsichtbar
+        button.style.cursor = 'pointer';
+    }
+
     // Positionen setzen und unsichtbar machen
     mappe.style.top = '100px';
     mappe.style.left = '50px';
