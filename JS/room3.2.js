07a22eaf6e7aab0520b66b72a61dd554a191a74a
Echoezvr <schojona@kkg-do.eu>
2025-06-26 17:20:56 +0200

    linked roterbutton and roterbuttonending (funktioniert aber buttons bitte Invisible machen und richtig placen

--- JS/room3.2.js
+++ JS/room3.2.js
@@ -2,3 +2,6 @@ function goToSeiteAdventureStart(){
     localStorage.setItem("showAlert", "true");
     window.location.href = "adventureStart.html";
 }
+function goToSeiteRedButtonEnding(){
+    window.location.href = "EndingRoterButton(ArbeiteBeiAmBROsius).html"
+}
