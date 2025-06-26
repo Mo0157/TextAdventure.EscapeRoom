028d51da2a254e788192b0a7ad0d4174f2f3a51a
Mo <shecmoha@kkg-do.eu>
2025-06-26 20:15:56 +0200

    Button in room3.html fixed

--- JS/room3.js
+++ JS/room3.js
@@ -1,28 +1,10 @@
 alert ("Du hast es in den dritten Raum geschafft! Du bist in einem Labor und siehst vor dir einen Computer. Probier rum!");
 function positioniereButtons() {
     const mappe = document.getElementById('mappe');
-
-    function unsichtbarmachen(button) {
-        button.style.position = 'absolute';
-        button.style.width = '100px';
-        button.style.height = '100px';
-        button.style.backgroundColor = 'transparent'; // unsichtbar
-        button.style.border = 'none';
-        button.style.opacity = '0'; // komplett unsichtbar
-        button.style.cursor = 'pointer';
-    }
-
-    // Positionen setzen und unsichtbar machen
-    mappe.style.top = '100px';
-    mappe.style.left = '50px';
-    mappe.style.position = 'absolute';
-    unsichtbarmachen(mappe);
 }
 
-function gotoseite3.2(){
+function gotoseite32(){
     alert("Du hast es in den dritten Raum geschafft! Du bist in einem Labor und siehst vor dir einen Computer. Probier rum!");
 
-    document.getElementById("mappe").addEventListener("click", function() {
-        window.location.href = "../HTMLroom3.2.html";
-    });
+        window.location.href = "../HTML/room3.2.html";
 }
